f29a31520b3f8c8597f07a57ce424e9474be4ce9
Metajjj <joolsjannaway@gmail.com>
2025-06-07 23:25:54 +0100

    adjusting anim

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     logLevel: 'warn',
 
     base:'/Portfolio/', //base name of url for gh-pages to work?
+    //base:'localhost:3000', //for local dev test
     build: {
         outDir: 'docs' //Set npm run build path for gh-pages
     }
